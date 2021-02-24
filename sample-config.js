c943132a6f3ef0dc2bf3c48ffceeb01c17f9685b
baadshah797 <79595399+baadshah797@users.noreply.github.com>
2021-02-24 23:07:50 +0530

    Add files via upload

---
+++ sample-config.js
@@ -0,0 +1,23 @@
+var config = {
+	projectId: "recaptchaverification",
+  apiKey: "AIzaSyAXcK-UL4f1L_jTQFDiDmqcz9FZEAmcmFc",
+  authDomain: "recaptchaverification.firebaseapp.com",
+  databaseURL: "https://recaptchaverification.firebaseio.com",
+  storageBucket: "recaptchaverification.appspot.com",
+    messagingSenderId: "55216416866",
+    appId: "1:55216416866:web:b601ea66ca915b3ee2093b",
+    measurementId: "G-4LH1EFERHE"
+};
+firebase.initializeApp(config);
+
+
+// Google OAuth Client ID, needed to support One-tap sign-up.
+// Set to null if One-tap sign-up is not supported.
+var CLIENT_ID =
+    '935978992486-7noqfh4ue5e8an8mbbr3rd3m5cdn1u26.apps.googleusercontent.com';
+	
+	 
+    
+    
+    
+  
